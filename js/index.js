a8f6e9405cd22a76f9a59d5853e9c4949d9f12f7
Mensur Islami <mi@live.de>
2022-03-03 11:16:58 +0100

    misc

---
+++ js/index.js
@@ -0,0 +1,38 @@
+function ViewModel() {
+    var self = this;
+    self.pageTitle = ko.observable();
+    self.address = ko.observable();
+}
+
+function GetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+
+function GetData(){
+    const dataUrl = "https://raw.githubusercontent.com/mensaco/nagruj/main/data/doc.json";
+    GetAsync(dataUrl, function(data){
+
+        const d = JSON.parse(data);
+
+        var model = new ViewModel();
+        // Activates knockout.js
+        ko.applyBindings(model);
+
+        model.pageTitle(d["PageTitle"]);
+        model.address(d["Address"]);
+
+    });
+}
+
+GetData();
+
+
+
