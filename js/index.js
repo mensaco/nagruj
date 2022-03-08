b674099b122531a6e13fb86900011741f86137b9
Mensur Islami <mi@live.de>
2022-03-08 20:36:51 +0100

    seems good. remains formatting the Impressum

--- js/index.js
+++ js/index.js
@@ -4,25 +4,29 @@ function ViewModel() {
     // from the json file
 
     var self = this;
-    self.showLanding = function(){
-        alert('Yaaay!');
+    self.isImpressum = ko.observable(false);
+    self.showImpressum = function () {
+        self.isImpressum(true);
+        return true;
+    }
+    self.hideImpressum = function () {
+        self.isImpressum(false);
+        return true;
     }
 
-    
 }
 
 
-function GetKeyAndValue(prop){
-    return { 
+function GetKeyAndValue(prop) {
+    return {
         key: Object.keys(prop)[0],
         value: Object.values(prop)[0]
     };
 }
 
-function GetAsync(theUrl, callback)
-{
+function GetAsync(theUrl, callback) {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
+    xmlHttp.onreadystatechange = function () {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
             callback(xmlHttp.responseText);
     }
@@ -33,10 +37,10 @@ function GetAsync(theUrl, callback)
 function htmlToElement(html, attributes) {
     var template = document.createElement('template');
     html = html.trim(); // Never return a text node of whitespace as the result
-    
+
 
     // add attributes if present
-    if(attributes && attributes.length > 0){
+    if (attributes && attributes.length > 0) {
         var arrat = [];
         for (let i = 0; i < attributes.length; i++) {
             const a = attributes[i];
@@ -56,30 +60,30 @@ function htmlToElement(html, attributes) {
 }
 
 // allow for using templated controls
-function updateTemplates(){
+function updateTemplates() {
 
     var templates = document.querySelectorAll("template");
-    templates.forEach(c => {        
+    templates.forEach(c => {
         var template = c.innerHTML;
         var tag = template.split(/[\{\}]/)[1];
         var components = document.querySelectorAll(tag);
-         components.forEach(t => {
+        components.forEach(t => {
             var content = t.innerHTML;
-            var newHtml = template.replace("{"+tag+"}", content);
+            var newHtml = template.replace("{" + tag + "}", content);
             var newElement = htmlToElement(newHtml, t.attributes);
             t.parentElement.replaceChild(newElement, t);
         });
     });
-    
+
 
 }
 
 
 
 
-function GetData(){
+function GetData() {
     const dataUrl = "data/doc.json?v=2";
-    GetAsync(dataUrl, function(data){
+    GetAsync(dataUrl, function (data) {
 
         // fix DOM before processing any data
         updateTemplates();
@@ -92,23 +96,21 @@ function GetData(){
 
         // extract defined keys for the model
         var jsonKeys = Object.keys(d);
-        
+
         // define and initialize each key ...
         jsonKeys.forEach(k => {
 
-            if(typeof d[k] == 'string'){
+            if (typeof d[k] == 'string') {
                 model[k] = ko.observable(d[k]); // .. as ko Observable 
             }
-            else if(Array.isArray(d[k])){
+            else if (Array.isArray(d[k])) {
                 model[k] = ko.observableArray(d[k]); // .. as ko Observable Array
             }
-            else{
-                
+            else {
+                model[k] = ko.observable(d[k]); // .. as ko Observable 
             }
         });
 
-        
-        
         // apply bindings
         ko.applyBindings(model);
 
